829cf43ac607f6c87f2fdae626d79e5ebfbc76e6
LookItsAndy <59989200+LookItsAndy@users.noreply.github.com>
2022-02-11 07:45:56 -0800

    added rainbow effect
    from the led exmaple

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,6 +20,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 public class Robot extends TimedRobot {
   private AddressableLED m_led;
   private AddressableLEDBuffer m_ledBuffer;
+  private int m_rainbowFirstPixelHue;
 
 
   /**
@@ -40,7 +41,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     
-    setStaticColor();
+    rainbow();
     m_led.setData(m_ledBuffer);
   }
 
@@ -51,6 +52,22 @@ public class Robot extends TimedRobot {
       m_ledBuffer.setRGB(i, RobotPreferences.ColorPrefs.redPref.getValue(), RobotPreferences.ColorPrefs.greenPref.getValue(), RobotPreferences.ColorPrefs.bluePref.getValue());
 
     }
+  }
+
+
+  private void rainbow() {
+    // For every pixel
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+      // Set the value
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    m_rainbowFirstPixelHue += 3;
+    // Check bounds
+    m_rainbowFirstPixelHue %= 180;
   }
 
   
